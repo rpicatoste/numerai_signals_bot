91017b69480fa75b8127cfe74c5b3ec3e20f4f5b
Servando <vandotorresg@gmail.com>
2022-10-10 22:22:51 +0200

    pre-commit with basic

--- src/data/database_generator.py
+++ src/data/database_generator.py
@@ -10,7 +10,8 @@ class DataBaseGenerator:
     Class to manage the database.
     It will connect to the sources and get the data. Then it will store it in a database.
     """
-    def __init__(self, db_cfg_path: Union[Path, str]):
+    def __init__(self, 
+    db_cfg_path: Union[Path, str]):
         self.cfg = DataBaseConfiguration(db_cfg_path)
 
     def create_initial_database(self):
